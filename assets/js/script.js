2cf3b8226498eff8f251d80818f7c67a6ae9efde
Matt Weichel <maweiche@gmail.com>
2021-09-28 17:02:43 -0700

    fixed scoring issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//------------- VARIABLES 
 var qText = document.querySelector(".question-words");
 var qOption = document.querySelector(".option")
 var score = document.querySelector(".score");
@@ -8,24 +9,20 @@ var nextButton = document.getElementsByClassName("next")[0];
 var verdict = document.getElementsByClassName("verdict");
 var userName = document.querySelector(".userName");
 var scoreToRecord = document.querySelector(".scoreToRecord");
-var saveScores = document.getElementsByClassName(".saveScores");
+var saveScoreButton = document.getElementById("submitScoreButton");
 var resetButton = document.getElementById(".resetButton");
 var highScores = document.querySelector(".highScores");
 var highScoreBox = document.getElementById("highscoreContainer");
 var currentScoreEl = document.getElementById("currentScore");
+var loseGameScreenEl = document.getElementById("loseGameScreenContainer")
 
 var scoreCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
+var selected ;
+var id = 0;
 
-document.querySelector("#op1").style.visibility = "hidden";
-document.querySelector("#op2").style.visibility = "hidden";
-document.querySelector("#op3").style.visibility = "hidden";
-document.querySelector("#op4").style.visibility = "hidden";
-document.querySelector(".submit").style.visibility  = "hidden";
-
-// document.querySelector(".highScores").style.visibility = "hidden";
 //Array of questions
 var questions = [
     {
@@ -72,6 +69,17 @@ var questions = [
 //New idea
 startButton.addEventListener("click", startQuiz);
 
+//Hide on start 
+document.querySelector("#op1").style.visibility = "hidden";
+document.querySelector("#op2").style.visibility = "hidden";
+document.querySelector("#op3").style.visibility = "hidden";
+document.querySelector("#op4").style.visibility = "hidden";
+document.querySelector(".submit").style.visibility  = "hidden";
+
+// document.querySelector(".highScores").style.visibility = "hidden";
+
+//------------------FUNCTIONS 
+
 //The loseGame function is called when timer reaches 0
 function loseGame() {
     qText.textContent = "The Quiz is now over!"
@@ -84,8 +92,10 @@ function loseGame() {
     // document.querySelector(".highScores").style.visibility = "visible";
     clearInterval(timer);
     currentScoreEl.textContent = scoreCounter;
-    var askInitials = prompt("Enter your initials.");
-    console.log(askInitials);
+    //Make it visible 
+    loseGameScreenEl.style.display = "flex"; 
+    // var askInitials = prompt("Enter your initials.");
+    // console.log(askInitials);
         // function storeScoreInput() {
         // var saveInput = {
         //     initials: askInitials.trim(),
@@ -127,7 +137,6 @@ function iterate(id) {
     op3.value = questions[id].a[2].isCorrect;
     op4.value = questions[id].a[3].isCorrect;
 
-    var selected = "";
 
     op1.addEventListener("click", () => {
         op1.style.backgroundColor = "pink";
@@ -151,6 +160,7 @@ function iterate(id) {
         op3.style.backgroundColor = "pink";
         op4.style.backgroundColor = "green";
         selected = op3.value;
+
     })
 
     op4.addEventListener("click", () => {
@@ -165,21 +175,25 @@ function iterate(id) {
 }
 submitButton[0].onclick = function(event) {
     id++;
-    console.log("clicking on submit btn");
+    console.log("clicking on submit btn", selected);
     if (id < 4) {
         iterate(id);    
         console.log(id);
     }
-    if (event.target[4] == "true") {
+    
+    if (selected === "true" ) {
         verdict[0].innerHTML = "Correct!"
         verdict[0].style.color = "green";
         scoreCounter++;
-        console.log(scoreCounter);
-    } if (event.target[4] == "false") {
+        console.log("scoreCounter", scoreCounter);
+    } //answer is false 
+    else if (selected === "false") {
         verdict[0].innerHTML = "Wrong!";
-        verdict[0].style.color = "Red!";
+        verdict[0].style.color = "red";
         timerCount -= 5;
-    }if (qText.textContent === "Game Over!"){
+        console.log("timerCount", timerCount, scoreCounter);
+    }
+    if (qText.textContent === "Game Over!"){
         loseGame();
     } if (id > 3){
         loseGame();
@@ -188,6 +202,25 @@ submitButton[0].onclick = function(event) {
     return;
     
 } 
+//Function to loseGameScreen
+// function loseGameScreen() {
+//     loseGameScreen.style.display = "flex";
+//     //hide question box??
+//     saveScore.addEventListener("click", function () {
+//         let scoresInput = { userName: nameInput.value.trim(), userScore: scoreCounter};
+//         //if it does not exist already, push first score
+//         if (typeof localStorage.getItem("scores" == "object") {
+//             scoresArray.push(scoresObject);
+//             localStorage.setItem("scores", JSON.stringify(scoresArray));
+//             //if does not exist pull scores, push ne
+//         } else {
+//             var pulledScores = JSON.parse(localStorage.getItem("scores"));
+//             pulledScores.push(scoresObject);
+//             localStorage.setItem("scores", JSON.stringify(pulledscores));
+//         })
+//     })
+// }
+
 
 //Function for if Quiz is started 
 function startQuiz () {
@@ -236,237 +269,8 @@ function startTimer() {
   }
 };
 
-var id = 0;
 
 
 
 
 
-
-
-
-
-////HIGH SCORE AREA/////////////////////
-// const  NUMBOFHIGHSCORES = 5;
-// const  HIGHSCORES = "highScores";
-
-// const highScoreString = localStorage.getItem(HIGHSCORES);
-// const highScores = JSON.parse(highScoreString) ?? [];
-
-// const lowestScore = highScores [NUMBOFHIGHSCORES - 1]?.score ?? 0;
-
-// function checkHighScore(score) {
-//     const  highScores = JSON.parse(localStorage.getItem(HIGHSCORES)) ?? [];
-//     const lowestScore = highScores[NUMBOFHIGHSCORES - 1]?.score ?? 0;
-
-//     if (score > lowestScore) {
-//         saveHighScore(score, highScores);
-//         showHighScores();
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-// nextButton.addEventListener("click", () => {
-//     start = false;
-    
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //The ini function is called when the page loads
-// function init() {
-//     getHighScores();
-// }
-
-// //The startGame function is called when the start button is clicked
-// function startGame() {
-//     isWin = false;
-//     timerCount = 10;
-//     //Prevents start butto from being clicked when round is in progress
-//     startButton.disabled = true;
-//     renderQText()
-//     startTimer()
-// }
-
-// //The gameOver function is called when the game completion condition is met
-// function gameOver() {
-//     qText.textContent = "The Quiz is now over!"
-//     // scoreCounter
-//     startButton.disabled = false;
-//     setScore();
-// }
-
-// //The loseGame function is called when timer reaches 0
-// function loseGame() {
-//     qText.textContent = "The Quiz is now over!"
-//     //scoreCounter
-//     startButton.disabled = false;
-//     setScore();
-// }
-
-// function startTimer() {
-//     // Sets timer
-//     timer = setInterval(function() {
-//       timerCount--;
-//       timerEl.textContent = timerCount;
-//     //   if (timerCount >= 0) {
-//     //     // Tests if win condition is met
-//     //     if (isWin && timerCount > 0) {
-//     //       // Clears interval and stops timer
-//     //       clearInterval(timer);
-//     //       winGame();
-//     //     }
-//     //   }
-//       // Tests if time has run out
-//       if (timerCount === 0) {
-//         clearInterval(timer);
-//         loseGame();
-//       }
-//     }, 1000);
-//   }
-
-// //The startTimer function starts and stops the timer and triggers gameOver
-// // function startTimer() {
-// //     //Sets timer
-// //     timer = setInterval(function() {
-// //         timerCount--;
-// //         timerEl.textContent = timerCount;
-// //         if (timerCount >= 0) {
-// //             //Tests if win condition is met
-// //             if (isWin && timerCount > 0) {
-// //                 //Clears interval and stops timer
-// //                 clearInterval(timer);
-// //                 gameOver();
-// //             }
-// //         }
-// //         //Tests if time has run out
-// //         if (timerCount === 0) {
-// //             clearInverval(timer);
-// //             loseGame();
-// //         }
-// //      }, 1000);
-// // }
-// //Creates question text on screen
-// function renderQText(index) {
-//     //Picks question from question array
-//     chosenQ = questions[Math.floor(Math.random() * questions.length)];
-//     qText.textContent = chosenQ.question;
-//     // qOptions.textContent = chosenQ.options[0],chosenQ.options[1],chosenQ.options[2],chosenQ.options[3];
-//     var chosenQOptions = [chosenQ.options[0], chosenQ.options[1], chosenQ.options[2],chosenQ.options[3]];
-
-//     function dispOptions () {
-//         // var more=document.getElementById("more");
-//         for (var i=0; i < chosenQOptions.length; i++) {
-//             var butt=document.createElement("button");
-//             butt.innerHTML=chosenQOptions[i];
-//             qOptions.appendChild(butt);  
-//         }
-
-//         qOptions.addEventListener("click", function(event) {
-//             var clickedOption = event.target;
-//             if (clickedOption = chosenQ.answer){
-//                 score++;
-//             }
-//             console.log(clickedOption);
-//             chosenQOptions = []; // need to empty question options array somehowx`
-//         });
-//     }        dispOptions();
-//     }
-
-
-//     // var queTag = "Question" + ":" +questions[index].question[2];
-// //     var optionTag = '<div class="option"><span>'+ questions[index].options[0]+'</span></div>'
-// //     + '<div class="option"><span>'+ questions[index].options[1]+'</span></div>'
-// //     + '<div class="option"><span>'+ questions[index].options[2]+'</span></div>'
-// //     + '<div class="option"><span>'+ questions[index].options[3]+'</span></div>';
-// //     qText.textContent = queTag;
-// //     options.textContent = optionTag;
-
-// //     var option = options.querySelectorAll(".option");
-
-// //     for(i=0; i < options.length; i++){
-// //         option[i].setAttribute("onclick", "optionSelected(this)");
-// //     }
-// // }
-
-// //Updates scores on screen and sets score and initals to client storage
-// function setScore() {
-//     score.textContent = scoreCounter;
-//     localStorage.setItem("scoreCount", scoreCounter);
-// }
-
-// //These functions are used by init
-// function getHighScores() {
-//     //Get stored value from client storage, if it exists
-//     var storedScores = localStorage.getItem("scoreCount");
-//     //If stored value doesn't exist, set counter to blank
-//     if (storedScores === null) {
-//         scoreCounter = "";
-//     } else {
-//         //If a value is retrieved from client storage then display it
-//         scoreCounter = storedScores;
-//     }
-//     //Render scores to page
-//     score.textContent = scoreCounter;
-// }
-
-// function checkWin() {
-//     //If the button selected is the correct answer then set isWin to true
-//     if (chosenQ.answer.textContent = clickedOption) {
-//         //This value is used in the timer function to test if gameOver condition is met
-//         scoreCounter++;
-//     }
-// }
-// // Tests if guessed selection is the rightAnswer
-// // function checkAnswer(selection) {
-// //     var userSelection = false;
-// //     for (var i = 0; i < 
-// // }
-
-// //Attach event listener to start button to call startGame function on click
-// document.addEventListener("click", startGame);
-
-// //Calls init() so that it fires when page opens
-// init();
-
-// //Add reset button
-// var resetButton = document.querySelector(".reset-button");
-
-// function resetGame() {
-//     scoreCounter = 0;
-//     //Renders score and sets it into the client storage
-//     setScore()
-// }
-// //Attaches event listener to button
-// resetButton.addEventListener("click", resetGame);
